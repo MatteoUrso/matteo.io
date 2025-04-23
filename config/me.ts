31deae19893fc1c33d409e04e7aa1f2a4589ec90
UfoTheKing <matteourso100@gmail.com>
2025-04-23 22:19:40 +0200

    Refactor styles in globals.css and implement utility function for class names; add personal information in me.ts

---
+++ config/me.ts
@@ -0,0 +1,12 @@
+export const Me = {
+  firstName: "Matteo",
+  lastName: "Urso",
+  fullName: "Matteo Urso",
+
+  socials: {
+    github: "https://github.com/MatteoUrso",
+    linkedIn: "https://www.linkedin.com/in/matteo-urso/",
+    twitter: "https://x.com/MatteoUrso2002",
+    instagram: "https://www.instagram.com/ursomatteo_/",
+  },
+};
